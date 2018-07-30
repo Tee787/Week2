da4d80402f4b8f6dfd90cc21a1719fecf3e013f5
Tee787 <tawhiri.tamihana@gmail.comgit config --global user.name Tawhirimatea>
2018-07-31 05:45:59 +1200

    second commit

---
+++ HelloName/HelloName/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b, c, d;
+            Console.WriteLine("Hello, please enter your name : ");
+            a = Console.ReadLine();
+            Console.WriteLine("Enter your age :  ");
+            b = Console.ReadLine();
+            Console.WriteLine("What is your agenda : ");
+            c = Console.ReadLine();
+            Console.WriteLine("Enter your address : ");
+            d = Console.ReadLine();
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
